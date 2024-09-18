7d3e593039ea76a2c0ff60b042b885cefffac4e9
GeroGili <geronimo.giliberti@davinci.edu.ar>
2024-09-18 18:28:48 -0300

    estado

--- src/Ejer_3/prestamo.java
+++ src/Ejer_3/prestamo.java
@@ -1,5 +1,7 @@
 package Ejer_3;
 
+import javax.swing.JOptionPane;
+
 public class prestamo {
 	
 	    private String[] titulos;
@@ -11,6 +13,15 @@ public class prestamo {
 	            "libro2"
 	        };
 	        disponibles = new boolean[]{true, true}; 
+	    }
+	    public void consultarEstado() {
+	        String mensaje = "Estado de los Libros:\n";
+	        for (int i = 0; i < titulos.length; i++) {
+	            String estado = disponibles[i] ? "disponible" : "no disponible";
+	            mensaje += titulos[i] + " - " + estado + "\n"; 
+	            JOptionPane.showMessageDialog(null, mensaje);
+	        }
+	      
 	    }
 
 
