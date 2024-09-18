a2f02545085bce32bf7a6cf38105d5b9841fabd4
GeroGili <geronimo.giliberti@davinci.edu.ar>
2024-09-18 18:18:37 -0300

    arranque2

---
+++ src/Ejer_3/prestamo.java
@@ -0,0 +1,17 @@
+package Ejer_3;
+
+public class prestamo {
+	
+	    private String[] titulos;
+	    private boolean[] disponibles;
+	    
+	    public prestamo() {
+	        titulos = new String[]{
+	            "libro1",
+	            "libro2"
+	        };
+	        disponibles = new boolean[]{true, true}; 
+	    }
+
+
+}
