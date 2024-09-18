1aa1cafeca40f9faa8c01ef8b9ea8897347e81a4
GeroGili <geronimo.giliberti@davinci.edu.ar>
2024-09-18 18:53:33 -0300

    botonera

--- src/Ejer_3/main2.java
+++ src/Ejer_3/main2.java
@@ -1,9 +1,41 @@
 package Ejer_3;
 
+import javax.swing.JOptionPane;
+
 public class main2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		prestamo prestamo = new prestamo();
+        int opcion;
+        String[] opciones = {"Mire el estado","Prestacion de libro","Devolver libro", "salir"};
+        do {
+        	
+			opcion = JOptionPane.showOptionDialog(null,  
+				"Bienvenido a Lectores Felices",
+				"",
+				0, 
+				0, 
+				null, 
+				opciones, 
+				opciones[1] );	          	      
+            switch (opcion) {
+                case 0:
+                	prestamo.consultarEstado();
+                    break;
+                case 1:
+                	prestamo.prestarLibro();
+                    break;
+                case 2:
+                	prestamo.devolverLibro();
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Que tenga un buen dia");
+                    break;
+               
+            }
+        } while (opcion != 3);
+		
+		
 
 	}
 
